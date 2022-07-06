17e6e55302ff310c88b8b2ba299988a35a611f01
hope <proftoby4real@hotmail.com>
2022-07-06 05:32:30 +0100

    Changed Routing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Main',
+        permanent: true,
+      },
+    ]
+  },
+  reactStrictMode: true
+}
